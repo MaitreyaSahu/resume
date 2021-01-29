ac9695f6d031b22c276db3eefa998a6d1bdd6f8e
Maitreya <mtsahu1995@gmail.com>
2021-01-30 02:18:39 +0530

    added git repos using vue.js

---
+++ scripts/vue.js
@@ -0,0 +1,51 @@
+Vue.component("project", {
+    props: ["project"],
+    template: `
+            
+            <section class="project-item">
+            <img class="project-img" src={{project.name}} alt=project.name{{project.name}}>
+            <div>
+                <h3>
+                    <a href="#">{{project.name}}</a>
+                    <!-- <a href="https://maitreyasahu.github.io/TicTacToe/" target="_blank">Tik Tak Toe</a> -->
+                </h3>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima, perferendis, culpa eum
+                    tempora
+                    fuga
+                    cum accusantium numquam unde quod veniam obcaecati perspiciatis similique animi iste soluta
+                    natus!
+                    Voluptate, expedita ratione!</p>
+                <ul class="project-tech">
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JavaScript</li>
+                </ul>
+                <a class="btn" href="#">Source Code</a>
+            </div>
+        </section>
+    `
+});
+
+new Vue({
+    el: "#git-projects",
+    data: {
+        projects: []
+    },
+    created: function () {
+        // Alias the component instance as `vm`, so that we
+        // can access it inside the promise function
+        var vm = this;
+        // Fetch our array of posts from an API
+        fetch("https://api.github.com/users/MaitreyaSahu/repos")
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                vm.projects = data.filter(project => (project.homepage != null));
+                console.log(data);
+            });
+    }
+});
+
+
+
