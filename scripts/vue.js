061a2756759b93224e6b78753d80a4c32fbcb9f5
Maitreya <mtsahu1995@gmail.com>
2021-01-30 13:40:32 +0530

    added somo icons

--- scripts/vue.js
+++ scripts/vue.js
@@ -16,9 +16,9 @@ Vue.component("project", {
                     natus!
                     Voluptate, expedita ratione!</p>
                 <ul class="project-tech">
-                    <li>HTML</li>
-                    <li>CSS</li>
-                    <li>JavaScript</li>
+                    <li><ion-icon name="logo-html5"></ion-icon></li>
+                    <li><ion-icon name="logo-css3"></ion-icon></li>
+                    <li><ion-icon name="logo-javascript"></ion-icon></li>
                 </ul>
                 <a class="btn" href="#">Source Code</a>
             </div>
